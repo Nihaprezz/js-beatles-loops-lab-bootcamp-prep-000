13528ea7fc1b6e793d22a48a1b378c4d796bac83
Sergio Perez <sergio13prez@gmail.com>
2019-07-22 00:17:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatsPlay(arrayMusicians, arrayInstruments){
+  var testArray = [];
+  for (var i = 0; i < arrayMusicians.length; i++){
+    testArray[i] = `${arrayMusicians[i]} plays ${arrayInstruments}`
+  }
+ return testArray
+}
+
+function johnLennonFacts(array){
+  var i =0;
+  while (i < array.length){
+    console.log(array[i]);
+  }
+}
+
+johnLennonFacts([1,2,3])
